0b2424a0df66237d50c72ac2c360a8a6bc925d32
takizawa0548 <takizawa020383461@gmail.com>
2022-03-20 14:44:35 +0900

    fast

---
+++ db/init/02_insert_date.sql
@@ -0,0 +1,5 @@
+INSERT INTO books VALUES(1, 'あ', current_timestamp);
+INSERT INTO books VALUES(2, 'い', current_timestamp);
+INSERT INTO books VALUES(3, 'う', current_timestamp);
+INSERT INTO books VALUES(4, 'え', current_timestamp);
+INSERT INTO books VALUES(5, 'お', current_timestamp);
